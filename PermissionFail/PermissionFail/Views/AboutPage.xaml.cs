78bd7713649306a2d8ba163ddf728ae32660b0f6
Will Autio <Will.Autio@i-trax.solutions>
2020-05-04 17:06:01 -0700

    Add project files.

---
+++ PermissionFail/PermissionFail/Views/AboutPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace PermissionFail.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            PermissionStatus status5 = await Permissions.CheckStatusAsync
+                <Permissions.LocationWhenInUse>().ConfigureAwait(false);
+
+            TestLabel.Text = "Something";
+        }
+
+       }
+}
